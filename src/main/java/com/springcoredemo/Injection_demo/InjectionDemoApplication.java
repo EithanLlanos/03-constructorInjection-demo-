a85507fd4978308877f86356c08e258b6b297a21
Rubick <eithanllanosb@gmail.com>
2024-11-23 21:20:38 -0500

    First commit

---
+++ src/main/java/com/springcoredemo/Injection_demo/InjectionDemoApplication.java
@@ -0,0 +1,13 @@
+package com.springcoredemo.Injection_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InjectionDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InjectionDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springcoredemo/Injection_demo/InjectionDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springcoredemo.Injection_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InjectionDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
