0e8cde48ee0916ba8d6d7f2e49375d16d87f00d5
Rubick <eithanllanosb@gmail.com>
2024-11-29 17:35:39 -0500

    Defining field injection (deprecated)

--- src/main/java/com/springcoredemo/Injection_demo/rest/DemoController.java
+++ src/main/java/com/springcoredemo/Injection_demo/rest/DemoController.java
@@ -8,16 +8,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DemoController {
 
-    // define a private field for the dependency
-    private Coach myCoach;
+//    // define a private field for the dependency
+//    private Coach myCoach;
+
 
 //    // define a constructor for dependency injection
 //    @Autowired
 //    public DemoController(Coach theCoach){ myCoach = theCoach;    }
 
-    // define a setter for setter injection
+//    // define a setter for setter injection
+//    @Autowired
+//    public void setCoach(Coach theCoach){ myCoach = theCoach;}
+
+    // define the variable as Autowired for field injection (deprecated)
     @Autowired
-    public void setCoach(Coach theCoach){ myCoach = theCoach;}
+    private Coach myCoach;
+
 
     @GetMapping("/dailyworkout")
     public String getDailyWorkout(){
